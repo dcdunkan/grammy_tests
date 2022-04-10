00ad6e2632b411fc49640676e8bfeddc76d792ed
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-04-10 18:15:27 +0530

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export * as GrammyTypes from "https://cdn.skypack.dev/@grammyjs/types@v2.6.0?dts";
-export { Bot, Context } from "https://deno.land/x/grammy@v1.7.1/mod.ts";
+export { Bot, Context } from "https://deno.land/x/grammy@v1.7.2/mod.ts";
 export type {
   Methods,
   Payload,
   RawApi,
-} from "https://deno.land/x/grammy@v1.7.1/core/client.ts";
+} from "https://deno.land/x/grammy@v1.7.2/core/client.ts";
--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -3,4 +3,4 @@ export {
   Context,
   session,
   type SessionFlavor,
-} from "https://deno.land/x/grammy@v1.7.1/mod.ts";
+} from "https://deno.land/x/grammy@v1.7.2/mod.ts";
