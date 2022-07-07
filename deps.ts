dedf6da6dc305a6707dfab5f1f484db1ede223e2
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2022-07-07 15:58:26 +0530

    Remove internal import statements
    - ...and just copy from grammY source lol
    - related to https://github.com/grammyjs/i18n#20
    - result: no unnecessary mappings for dnt.

--- deps.ts
+++ deps.ts
@@ -1,7 +1,13 @@
+import { RawApi } from "https://lib.deno.dev/x/grammy@1.x/mod.ts";
+
+export type Methods<R extends RawApi> = string & keyof R;
+export type Payload<M extends Methods<R>, R extends RawApi> = M extends unknown
+  ? R[M] extends (signal?: AbortSignal) => unknown // deno-lint-ignore ban-types
+    ? {} // deno-lint-ignore no-explicit-any
+  : R[M] extends (args: any, signal?: AbortSignal) => unknown
+    ? Parameters<R[M]>[0]
+  : never
+  : never;
+
 export * as GrammyTypes from "https://esm.sh/@grammyjs/types@2.8.0";
 export { Bot, Context } from "https://lib.deno.dev/x/grammy@1.x/mod.ts";
-export type {
-  Methods,
-  Payload,
-  RawApi,
-} from "https://lib.deno.dev/x/grammy@1.x/core/client.ts";
